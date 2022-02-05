8886edb1a0d2ecb31f0fad4803a68c1f8cbcfbff
Priyansh Agrawal <w0rk5107h@w0rk5107h.com>
2022-02-05 14:15:46 +0530

    added welcome and setKeyMap

--- helperText.py
+++ helperText.py
@@ -1,14 +1,59 @@
 ## CONTAINS ONLY TEXT THAT NEED TO BE DISPLAYED ##
 
 welcomeMessage = '''
-                                  Welcome to
+                           Welcome to
+  ___ _     _   _        _          _      ___         _        _ _ 
+ / __| |___| |_| |_     /_\  _ _ __| |_   |_ _|_ _  __| |_ __ _| | |___ _ _ 
+ \__ \ / _ \  _| ' \   / _ \| '_/ _| ' \   | || ' \(_-<  _/ _` | | / -_) '_|
+ |___/_\___/\__|_||_| /_/ \_\_| \__|_||_| |___|_||_/__/\__\__,_|_|_\___|_|  
 
- __        __  ___               __   __                  __  ___                 ___  __ 
-/__` |    /  \  |  |__|     /\  |__) /  ` |__|    | |\ | /__`  |   /\  |    |    |__  |__)
-.__/ |___ \__/  |  |  |    /~~\ |  \ \__, |  |    | | \| .__/  |  /~~\ |___ |___ |___ |  \ 
+                         by w0rk5107h :)
 
 
-                                 by w0rk5107h :)
+Press:
+
+-> Enter to continue
+-> q to quit'''
+
+setKeyMapMessage = '''
+  ___      _     _  __           __  __           
+ / __| ___| |_  | |/ /___ _  _  |  \/  |__ _ _ __ 
+ \__ \/ -_)  _| | ' </ -_) || | | |\/| / _` | '_ \ 
+ |___/\___|\__| |_|\_\___|\_, | |_|  |_\__,_| .__/
+                          |__/              |_|   
+
+Your current key map is : ##keyMap##
+
+Press:
+
+-> Enter to continue
+-> c to change keymap
+-> Backspace to go back'''
+
+keyMapFileMessage = '''
+    _   _ _   _  __           __  __              
+   /_\ | | | | |/ /___ _  _  |  \/  |__ _ _ __ ___
+  / _ \| | | | ' </ -_) || | | |\/| / _` | '_ (_-<
+ /_/ \_\_|_| |_|\_\___|\_, | |_|  |_\__,_| .__/__/
+                       |__/              |_|      
+
+These are all the available keymaps, search your keymap and remember its index number.
+After finding your keymap press 'q' to go back.
 
 
-                             Press Enter to continue'''
+'''
+
+enterYourKeyMapMessage = '''
+  ___     _            __   __              _  __           __  __           
+ | __|_ _| |_ ___ _ _  \ \ / /__ _  _ _ _  | |/ /___ _  _  |  \/  |__ _ _ __ 
+ | _|| ' \  _/ -_) '_|  \ V / _ \ || | '_| | ' </ -_) || | | |\/| / _` | '_ \ 
+ |___|_||_\__\___|_|     |_|\___/\_,_|_|   |_|\_\___|\_, | |_|  |_\__,_| .__/
+                                                     |__/              |_|   
+
+Press:
+
+-> c to view keymaps again
+
+OR
+
+Enter the index number of your selected keymap : '''
--- installer.py
+++ installer.py
@@ -1,16 +1,99 @@
 import curses
 from curses import wrapper
 import helperText
+from subprocess import check_output
+from os import system
+
+TASKS = ['welcome', 'setKeyMap']
+TASK = 0
+
+def quit(stdscr):
+    pass
+
+# welcome screen
+def welcome(stdscr):
+    global TASK
+    global TASKS
+    
+    stdscr.clear()
+    stdscr.addstr(helperText.welcomeMessage)
+    stdscr.refresh()
+    while True:
+        key = stdscr.getkey()
+        if key == 'KEY_ENTER':
+            TASK += 1
+            break
+        elif key.lower() == 'q':
+            quit()
+            break
+
+# key map settings
+def setKeyMap(stdscr):
+    global TASK
+    global TASKS
+
+    keyMap = check_output("localectl status | awk -F ':' '{print $2}' | head -n 1").decode().strip()
+    stdscr.clear()
+    stdscr.addstr(helperText.setKeyMapMessage.replace('##keyMap##', keyMap))
+    stdscr.refresh()
+    while True:
+        key = stdscr.getkey()
+        if key == 'KEY_ENTER':
+            TASK += 1
+            break
+        elif key.lower() == 'c':
+            keyMaps = check_output('localectl list-keymaps', shell=True).decode().strip().split('\n')
+            f = open('keyMapFile','w+')
+            f.write(helperText.keyMapFileMessage)
+            count = 1
+            for keyMap in keyMaps:
+                f.write(f'{count}  -->  {keyMap}\n')
+                count += 1
+            f.close()
+            while key.lower() == 'c':
+                system('less keyMapFile')
+                stdscr.clear()
+                stdscr.addstr(helperText.enterYourKeyMapMessage)
+                stdscr.refresh()
+                index = ''
+                while True:
+                    key = stdscr.getkey()
+                    if key.lower() == 'c':
+                        break
+                    elif key == 'KEY_ENTER':
+                        if index.isdigit():
+                            index = int(index)
+                            if index >= 1 and index <= len(keyMaps)+1:
+                                pass
+                            else:
+                                key = 'c'
+                            break
+                        else:
+                            key = 'c'
+                            break
+                    elif key.isdigit():
+                        index += key
+                        stdscr.addstr(key)
+                        stdscr.refresh()
+            system(f'loadkeys {keyMaps[index-1]}')
+            TASK += 1
+            break
+        elif key.lower() == 'KEY_BACKSPACE':
+            TASK -= 1
+            break
 
 def main(stdscr):
+    global TASK
+    global TASKS
 
-    # FETCH THE SCREEN COORDINATES
-    MAX_Y = curses.LINES
-    MAX_X = curses.COLS
-
-    stdscr.clear()
-    stdscr.addstr((MAX_Y-11)//2,(MAX_X-11)//2, helperText.welcomeMessage)
-    stdscr.refresh()
+    
+    while True:
+        if TASKS[TASK] == 'welcome':
+            welcome(stdscr)
+        elif TASKS[TASK] == 'setKeyMap':
+            setKeyMap(stdscr)
+        elif TASK > len(TASKS):
+            break
     stdscr.getch()
 
 wrapper(main)
